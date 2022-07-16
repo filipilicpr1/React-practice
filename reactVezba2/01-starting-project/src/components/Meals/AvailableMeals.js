766b33c746b192e37e1b08963cda1c241404730d
filipilicpr1 <101191574+filipilicpr1@users.noreply.github.com>
2022-07-16 09:55:01 +0200

    Added food order react app

---
+++ reactVezba2/01-starting-project/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+  useEffect(() => {
+    // u use effect ne moze da se prosledi async funkcija, pa u funkciju koju prosledimo pravimo async funkciju koju pozovemo
+    async function fetchMeals() {
+      const response = await fetch(
+        "https://react-http-7b980-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+        setMeals(loadedMeals);
+        setIsLoading(false);
+      }
+    }
+    fetchMeals().catch((error) => { // ne moze preko try catch jer bacamo Error u async funkciju , pa ne radi lepo try catch , a ovo radi dobro
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading....</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
