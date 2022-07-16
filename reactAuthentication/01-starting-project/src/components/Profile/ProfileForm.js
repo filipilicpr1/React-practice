c8d5392a3bbfe3d3a9049fda19884146ce4d7367
filipilicpr1 <101191574+filipilicpr1@users.noreply.github.com>
2022-07-16 09:47:41 +0200

    Added simple react app with authentication

---
+++ reactAuthentication/01-starting-project/src/components/Profile/ProfileForm.js
@@ -0,0 +1,44 @@
+import classes from './ProfileForm.module.css';
+import { useRef, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
+import { useHistory } from 'react-router-dom';
+
+const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredPassword = newPasswordInputRef.current.value;
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCv6QxDJ60APZWnRcMixg1NlLAzIVo7sf4', {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredPassword,
+        returnSecureToken: false
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      history.replace('/');
+    });
+  }
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor='new-password'>New Password</label>
+        <input ref={newPasswordInputRef} type='password' minLength="7" id='new-password' />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+}
+
+export default ProfileForm;
---
+++ reactAuthentication/01-starting-project/src/store/auth-context.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect, useCallback } from "react";
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+let logoutTimer;
+
+function calculateRemainingTime(expirationTime) {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expirationTime).getTime();
+  const remainingDuration = adjExpirationTime - currentTime;
+  return remainingDuration;
+}
+
+function retrieveStoredToken() {
+  const storedToken = localStorage.getItem("token");
+  const storedExpirationTime = localStorage.getItem("expirationTime");
+  const remainingTime = calculateRemainingTime(storedExpirationTime);
+  if (remainingTime < 0) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTime");
+    return null;
+  }
+  return {
+    token: storedToken,
+    duration: remainingTime,
+  };
+}
+
+export const AuthContextProvider = (props) => {
+  const tokenData = retrieveStoredToken();
+  let initialToken;
+  if (tokenData) {
+    initialToken = tokenData.token;
+  }
+  const [token, setToken] = useState(initialToken);
+
+  const userIsLoggedIn = !!token; // !! sluzi da token konvertujemo u bool
+
+  const logoutHandler = useCallback(() => {
+    setToken(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem('expirationTime');
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  }, []);
+
+  function loginHandler(token, expirationTime) {
+    setToken(token);
+    localStorage.setItem("token", token);
+    localStorage.setItem("expirationTime", expirationTime);
+    const remainingTime = calculateRemainingTime(expirationTime);
+    logoutTimer = setTimeout(logoutHandler, remainingTime);
+  }
+
+  useEffect(() => {
+    if(tokenData) {
+        logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+    }
+  } , [tokenData, logoutHandler]);
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
